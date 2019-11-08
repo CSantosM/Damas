950be3f9a4c1a0da575d7aaa238454d32c0602e4
Carlos <4a.santos@gmail.com>
2019-11-08 18:49:24 +0100

    Removed session class

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/StartControllerTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/StartControllerTest.java
@@ -4,18 +4,19 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import es.urjccode.mastercloudapps.adcs.draughts.models.Session;
+import es.urjccode.mastercloudapps.adcs.draughts.models.Game;
+import es.urjccode.mastercloudapps.adcs.draughts.models.State;
 import es.urjccode.mastercloudapps.adcs.draughts.models.StateValue;
 
 public class StartControllerTest {
 
      @Test
     public void givenStartControllerWhenStartGameThenChangeState() {
-        Session session = new Session();
-        StartController startController = new StartController(session);
-        assertEquals(StateValue.INITIAL, session.getValueState());
+        State state = new State();
+        StartController startController = new StartController(new Game(),state);
+        assertEquals(StateValue.INITIAL, state.getValueState());
         startController.start();
-        assertEquals(StateValue.IN_GAME, session.getValueState());
+        assertEquals(StateValue.IN_GAME, state.getValueState());
     }
 
 }
