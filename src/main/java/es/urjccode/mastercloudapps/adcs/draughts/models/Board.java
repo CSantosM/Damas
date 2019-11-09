25c54b2c736b73cb957e5666d67549d2a3eba4ad
Carlos <4a.santos@gmail.com>
2019-11-09 11:37:06 +0100

    Fixed getPieces method

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Board.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Board.java
@@ -43,7 +43,7 @@ class Board {
     boolean isEmpty(Coordinate coordinate) {
         return this.getSquare(coordinate).isEmpty();
     }
-    
+
     Color getColor(Coordinate coordinate) {
         return this.getSquare(coordinate).getColor();
     }
@@ -52,12 +52,15 @@ class Board {
         List<Piece> pieces = new ArrayList<Piece>();
         for (int i = 0; i < this.getDimension(); i++) {
             for (int j = 0; j < this.getDimension(); j++) {
-                pieces.add(this.squares[i][j].getPiece());
+                Piece piece = this.getPiece(new Coordinate(i, j));
+                if (piece != null && piece.getColor() == color) {
+                    pieces.add(piece);
+                }
             }
         }
 		return pieces;
 	}
-    
+
     int getDimension() {
 		return Board.DIMENSION;
 	}
